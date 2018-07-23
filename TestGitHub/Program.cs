7a702e1c0b94e1b1f48996835b839370d46fad35
Vishwajeet Bose <vbose@helm360.com>
2018-07-23 16:37:50 +0530

    Add project files.

---
+++ TestGitHub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitHub
+{
+    class Program
+    {
+
+        static string Show()
+        {
+            return "Master Branch";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Show());
+            Console.ReadKey();
+        }
+    }
+}
